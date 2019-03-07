3cb2cdf30cda1a0870419a0a4addb5c14f870469
AJISUBHA123 <42169827+AJISUBHA123@users.noreply.github.com>
2019-03-07 10:59:23 +0530

    Update vc.py

--- vc.py
+++ vc.py
@@ -1,5 +1,8 @@
-ch=input("enter the character:")
-if(ch=='a' or ch=='e' or ch='i' or ch='o' or ch='u' or ch=='A' or ch=='E' or ch='I' or ch='O' or ch='U'):
-        print('the given character',ch,'is vowel')
+chr=input()
+if((chr>='a' and chr<='z') or (chr>='A' and chr<='Z')):
+	if(chr=='a' or chr=='e' or chr=='i' or chr=='o' or chr=='u' or chr=='A' or chr=='E' or chr=='I' or chr=='O' or chr=='U'):
+		print("Vowel")
+	else:	 
+		print("Consonant")
 else:
-    print('the given character',ch,'is consonant')
+	print('invalid')

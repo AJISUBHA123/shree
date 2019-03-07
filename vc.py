443fb8f714a99be26d6b3f25216167382ac358ee
AJISUBHA123 <42169827+AJISUBHA123@users.noreply.github.com>
2019-03-07 10:28:34 +0530

    Create vc.py

---
+++ vc.py
@@ -0,0 +1,5 @@
+ch=input("enter the character:")
+if(ch=='a' or ch=='e' or ch='i' or ch='o' or ch='u' or ch=='A' or ch=='E' or ch='I' or ch='O' or ch='U'):
+        print('the given character',ch,'is vowel')
+else:
+    print('the given character',ch,'is consonant')
